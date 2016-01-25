6767abef88d2b78c05b8c0edb67ca28e72348f6f
Marcelo Júnior <maljunior@gmail.com>
2016-01-25 19:05:09 -0300

    fix call to hash_error method; fix private_key validation

--- lib/recaptcha_plugin.rb
+++ lib/recaptcha_plugin.rb
@@ -20,9 +20,11 @@ class RecaptchaPlugin < Noosfero::Plugin
     msg_icve = _('Internal captcha validation error')
     msg_erpa = 'Environment recaptcha_plugin_attributes'
 
-    return RecaptchaVerification.hash_error(msg_icve, status, nil, "#{msg_erpa} private_key not defined") if private_key.nil?
-    return RecaptchaVerification.hash_error(msg_icve, status, nil, "#{msg_erpa} version not defined") unless version == 1 || version == 2
     rv = RecaptchaVerification.new
+
+    return rv.hash_error(msg_icve, status, nil, "#{msg_erpa} private_key not defined") unless private_key.present?
+    return rv.hash_error(msg_icve, status, nil, "#{msg_erpa} version not defined") unless version == 1 || version == 2
+
     if version  == 1
       verify_uri = 'https://www.google.com/recaptcha/api/verify'
       return rv.verify_recaptcha_v1(remote_ip, private_key, verify_uri, params[:recaptcha_challenge_field], params[:recaptcha_response_field])
